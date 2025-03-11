7a9d8e138b03f9e89398f10973e944abe9747a5b
Carlos Oscco <blustin2@gmail.com>
2025-03-11 16:20:13 -0400

    Refactor: Implement Blazor's built-in form validation in AskQuestion.razor

--- AssistClub.UI.Blazor/Models/QuestionRequest.cs
+++ AssistClub.UI.Blazor/Models/QuestionRequest.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AssistClub.UI.Blazor.Models;
 
 /// <summary>
@@ -8,23 +10,28 @@ public class QuestionRequest
     /// <summary>
     /// The unique identifier of the user submitting the question.
     /// </summary>
-    public Guid UserId { get; set; }
+    public Guid UserId { get; set; } = Guid.Parse("85300a85-112e-4410-a2b4-1cc079584347"); //change this to the actual user id when authentication is implemented
+    
     /// <summary>
     /// The title of the question.
     /// </summary>
+    [Required(ErrorMessage = "Title is required.")]
+    [StringLength(255, ErrorMessage = "Title must be at most 255 characters long.")]
     public string Title { get; set; }
     /// <summary>
     /// The full content of the question.
     /// </summary>
+    [Required(ErrorMessage = "Content is required.")]
+    [StringLength(2000, ErrorMessage = "Content must be at most 2000 characters long.")]
     public string Content { get; set; }
     /// <summary>
     /// Defines the visibility of the question (<c>public</c> or <c>private</c>).
     /// </summary>
-    public QuestionVisibility Visibility { get; set; }
+    public QuestionVisibility Visibility { get; set; } = QuestionVisibility.Public;
     /// <summary>
     /// The status of the question (<c>open</c>, <c>pending</c>, or <c>resolved</c>).
     /// </summary>
-    public string Status { get; set; }
+    public string Status { get; set; } = "open";
 }
 
 public enum QuestionVisibility
